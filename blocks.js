9f38d61d126e37c633e032c3384cdde88088011b
typeou <j.vincent@outlook.com>
2021-05-13 21:18:10 -0600

    Changing panel animation.

---
+++ blocks.js
@@ -0,0 +1,8 @@
+window.onload = function()
+{
+    $(".block").each(function(index)
+    {
+        $(this).css("animation-delay", (index * 0.25) + "s");
+        $(this).css("animation-name", "open" + (Math.floor(Math.random() * 2) + 1));
+    });
+}
