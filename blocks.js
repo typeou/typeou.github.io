9d38fe8d8834202849899f07eb8c76768883bde0
typeou <j.vincent@outlook.com>
2021-05-14 00:51:37 -0600

    A couple more variations.

--- blocks.js
+++ blocks.js
@@ -3,6 +3,6 @@ $(function()
     $(".block").each(function(index)
     {
         $(this).css("animation-delay", (index * 0.25) + "s");
-        $(this).css("animation-name", "open" + (Math.floor(Math.random() * 2) + 1));
+        $(this).css("animation-name", "open" + (Math.floor(Math.random() * 4) + 1));
     });
 });
